41e7b8cfdddf6730dc08acc4864f26eb04a82801
Patrick Brandt <patrick@fullstory.com>
2019-09-17 15:46:35 -0400

    adding TODO

--- .github/actions/sync-snippet-action/main.js
+++ .github/actions/sync-snippet-action/main.js
@@ -1,8 +1,6 @@
-const core = require('@actions/core');
+const core = require('@actions/core'); // https://github.com/actions/toolkit
 const github = require('@actions/github');
 const fs = require('fs');
-const { promisify } = require('util');
-const realPathSync = promisify(fs.realpath);
 const crypto = require('crypto');
 
 // read: https://github.com/actions/toolkit/tree/master/packages/github
@@ -10,8 +8,18 @@ const crypto = require('crypto');
 console.log(process.env.TEST);
 const run = async () => {
   const snippetText = fs.readFileSync(`./src/snippet.js`, 'utf-8');
-  const hash = crypto.createHash('md5').update(snippetText).digest('hex');
-  console.log(hash);
+  const localSnippetHash = crypto.createHash('md5').update(snippetText).digest('hex');
+  console.log(localSnippetHash);
+
+  // TODO:
+  // 1. Request snippet from app.fullstory.com/snippet
+  // 2. md5 hash the snippet from step 1
+  // 3. compare localSnippetHash with has from step 2
+  // 4. if they are the same, exit, if they are different:
+  // 5. update local snippet file with the content of the remote snippet
+  // 6. create a branch https://octokit.github.io/rest.js/#octokit-routes-git-create-ref
+  // 7. commit changes to the branch: https://octokit.github.io/rest.js/#octokit-routes-git-create-commit
+  // 8. Create a PR: https://octokit.github.io/rest.js/#octokit-routes-pulls-create
 }
 
 run();
