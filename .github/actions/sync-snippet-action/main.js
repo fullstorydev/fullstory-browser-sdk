1e24bfc6d38c2d7167b2a5d3193c8a6c0a1ccb37
Patrick Brandt <patrick@fullstory.com>
2019-09-17 14:39:10 -0400

    WIP: reading from core.input

---
+++ .github/actions/sync-snippet-action/main.js
@@ -0,0 +1,6 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+// read: https://github.com/actions/toolkit/tree/master/packages/github
+
+console.log(core.getInput('TEST'));
