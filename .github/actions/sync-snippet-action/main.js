52bda6a2200725cef3f227d467296e2cb6eb7f31
Patrick Brandt <patrick@fullstory.com>
2019-09-17 15:25:09 -0400

    exploring runtime env

--- .github/actions/sync-snippet-action/main.js
+++ .github/actions/sync-snippet-action/main.js
@@ -3,13 +3,14 @@ const github = require('@actions/github');
 const fs = require('fs');
 const { promisify } = require('util');
 const realPathSync = promisify(fs.realpath);
+const crypto = require('crypto');
 
 // read: https://github.com/actions/toolkit/tree/master/packages/github
 
 console.log(process.env.TEST);
 const run = async () => {
-  const pwd = await realPathSync('.');
-  const snippetText = fs.readFileSync(`${pwd}/src/snippet.js`, 'utf-8');
+  const snippetText = fs.readFileSync(`./src/snippet.js`, 'utf-8');
+  const hash = crypto.createHash('md5').update(snippetText).digest('hex');
   console.log(snippetText);
 }
 
