e726e8f6b1b50a918347f40428652ad4207c1a92
Patrick Brandt <patrick@fullstory.com>
2019-10-28 11:25:21 -0400

    retreiving remote snippet

--- .github/actions/sync-snippet-action/main.js
+++ .github/actions/sync-snippet-action/main.js
@@ -18,9 +18,9 @@ const run = async () => {
   const localSnippetHash = md5Hash(snippetText);
   console.log(`local snippet file hash: ${localSnippetHash}`);
 
-  const remoteSnippetText = await axios.get(SNIPPET_ENDPOINT);
-  console.log(`remoteSnippetText: ${remoteSnippetText.data}`);
-  const remoteSnippetHash = md5Hash(remoteSnippetText.data);
+  const remoteSnippetText = await axios.get(SNIPPET_ENDPOINT).data;
+  //console.log(`remoteSnippetText: ${remoteSnippetText}`);
+  const remoteSnippetHash = md5Hash(remoteSnippetText);
   console.log(`remote snippet file hash: ${remoteSnippetHash}`);
 
   console.log(`hashes equal? ${localSnippetHash === remoteSnippetHash}`);
