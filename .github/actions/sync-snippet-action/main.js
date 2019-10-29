77cadb9b2b586e78c455c574e7e6789e4fb5c4c4
Patrick Brandt <patrick@fullstory.com>
2019-10-29 13:47:03 -0400

    fixing linter issues

--- .github/actions/sync-snippet-action/main.js
+++ .github/actions/sync-snippet-action/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const core = require('@actions/core'); // https://github.com/actions/toolkit
 const github = require('@actions/github'); // https://github.com/actions/toolkit/tree/master/packages/github
 const fs = require('fs');
@@ -7,9 +8,7 @@ const axios = require('axios').default;
 const SNIPPET_PATH = 'src/snippet.js';
 const PR_TITLE = 'The FullStory snippet has been updated';
 
-const md5Hash = (text) => {
-  return crypto.createHash('md5').update(text).digest('hex');
-}
+const md5Hash = text => crypto.createHash('md5').update(text).digest('hex');
 
 const run = async () => {
   const localSnippetHash = md5Hash(fs.readFileSync(`./${SNIPPET_PATH}`, 'utf-8'));
@@ -17,7 +16,7 @@ const run = async () => {
 
   let remoteSnippetText;
   try {
-    remoteSnippetText = (await axios.get(process.env.SNIPPET_ENDPOINT)).data
+    remoteSnippetText = (await axios.get(process.env.SNIPPET_ENDPOINT)).data;
   } catch (e) {
     core.setFailed(e.message);
     throw e;
@@ -25,14 +24,14 @@ const run = async () => {
 
   const remoteSnippetHash = md5Hash(remoteSnippetText);
   console.log(`remote snippet file hash: ${remoteSnippetHash}`);
-  
-  if (localSnippetHash === remoteSnippetHash)  {
-    console.log('no changes to snippet'); 
+
+  if (localSnippetHash === remoteSnippetHash) {
+    console.log('no changes to snippet');
     return;
   }
 
   const branchName = `refs/heads/snippetbot/updated-snippet-${Date.now()}`;
-  const context = github.context;
+  const { context } = github;
   const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
 
   const repoInfo = {
@@ -46,7 +45,8 @@ const run = async () => {
   });
 
   console.log('checking for an on open snippet sync PR');
-  // NOTE: possible that searching for github-actions[bot] user might be too greedy. Assuming GH won't change this name.
+  // NOTE: possible that searching for github-actions[bot] user might be too greedy.
+  // Assuming GH won't change this name.
   const existingPR = openPRs.data.filter(pr => pr.title === PR_TITLE && pr.user.login === 'github-actions[bot]');
   if (existingPR.length > 0) {
     core.setFailed(`There is already an open PR for snippet syncronization. Please close or merge this PR: ${existingPR[0].html_url}`);
@@ -107,10 +107,11 @@ const run = async () => {
   await octokit.issues.addAssignees({
     ...repoInfo,
     issue_number: prResponse.data.number,
-    assignees: maintainers.filter(el => el !== prResponse.data.head.user.login), // avoid assigning the PR to the latest committer
+    // avoid assigning the PR to the latest committer
+    assignees: maintainers.filter(el => el !== prResponse.data.head.user.login),
   });
 
   console.log(`created PR: ${prResponse.data.html_url}`);
-}
+};
 
 run();
