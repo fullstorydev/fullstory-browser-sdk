d6a5c6f75dfb6c185db2e68541af446aad1337fb
Patrick Brandt <patrick@fullstory.com>
2019-10-29 01:02:26 -0400

    refactoring snippet path const

--- .github/actions/sync-snippet-action/main.js
+++ .github/actions/sync-snippet-action/main.js
@@ -5,13 +5,13 @@ const crypto = require('crypto');
 const axios = require('axios').default;
 
 const SNIPPET_ENDPOINT = 'http://dev-fs-com.s3-website-us-east-1.amazonaws.com/snippet.js';
-const LOCAL_SNIPPET = `./src/snippet.js`;
+const SNIPPET_PATH = 'src/snippet.js';
 
 const md5Hash = (text) => {
   return crypto.createHash('md5').update(text).digest('hex');
 }
 const run = async () => {
-  const localSnippetText = fs.readFileSync(LOCAL_SNIPPET, 'utf-8');
+  const localSnippetText = fs.readFileSync(`./${SNIPPET_PATH}`, 'utf-8');
   const localSnippetHash = md5Hash(localSnippetText);
   console.log(`local snippet file hash: ${localSnippetHash}`);
 
@@ -50,27 +50,27 @@ const run = async () => {
   });
   // console.log(`getTree response: ${JSON.stringify(getTreeResponse)}`);
 
-  const srcTree = getTreeResponse.data.tree.find(el => el.path === 'src/snippet.js');
+  const srcTree = getTreeResponse.data.tree.find(el => el.path === SNIPPET_PATH);
   // console.log(`srcTree: ${JSON.stringify(srcTree)}`);
 
   // https://octokit.github.io/rest.js/#octokit-routes-git-create-tree
   const treeResponse = await octokit.git.createTree({
     ...repoInfo,
     tree: [{
-      path: 'src/snippet.js',
+      path: SNIPPET_PATH,
       content: remoteSnippetText,
       mode: '100644',
       type: 'blob',
       base_tree: srcTree.sha,
     },
-    ...getTreeResponse.data.tree.filter(el => el.type === 'blob' && el.path !== 'src/snippet.js')]
+    ...getTreeResponse.data.tree.filter(el => el.type === 'blob' && el.path !== SNIPPET_PATH)]
   });
   //console.log(`tree response: ${JSON.stringify(treeResponse)}`);
 
   // https://octokit.github.io/rest.js/#octokit-routes-git-create-commit
   const commitResponse = await octokit.git.createCommit({
     ...repoInfo,
-    message: 'updated snippet.js',
+    message: `updated ${SNIPPET_PATH}`,
     tree: treeResponse.data.sha,
     parents: [context.sha],
   });
