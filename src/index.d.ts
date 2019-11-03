7d8d5f04e0ccb7bffda22b0373b6faa6fad553d4
Patrick Brandt <patrick@fullstory.com>
2019-11-03 10:18:07 -0500

    pruning a couple of functions not exposed directly from the snippet

--- src/index.d.ts
+++ src/index.d.ts
@@ -17,8 +17,6 @@ type LogLevel = 'log' | 'info' | 'warn' | 'error' | 'debug';
 // API functions that are available as soon as the snippet has executed.
 export function anonymize(): void;
 export function consent(userConsents?: boolean): void;
-export function disableConsole(): void;
-export function enableConsole(): void;
 export function event(eventName: string, eventProperties: { [key: string]: any }): void;
 export function identify(uid: string, customVars?: UserVars): void;
 export function init(options: SnippetOptions): void;
