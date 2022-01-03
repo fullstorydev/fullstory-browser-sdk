beb398a7436db054630dc8cd264061150411d3b0
Patrick@FullStory <45576380+patrick-fs@users.noreply.github.com>
2022-01-03 09:51:33 -0500

    Small doc fixup

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,7 @@
  * - orgId: Reference for your [Org Id](https://help.fullstory.com/hc/en-us/articles/360047075853) listed in FullStory.
  * - namespace: Global object name that contains the FullStory browser API methods and properties. Defaults to `FS`.
  * - debug: Debug mode with extra browser console logging.
- * - host: The recording server host domain. Can be set to direct recorded events to a proxy that you host. Defaults to 'fullstory.com'.
+ * - host: The recording server host domain. Can be set to direct recorded events to a proxy that you host. Defaults to `fullstory.com`.
  * - script: FullStory script host domain. FullStory hosts the `fs.js` recording script on a CDN, but you can choose to host a copy yourself. Defaults to `edge.fullstory.com`.
  * - recordCrossDomainIFrames: FullStory can record cross-domain iFrames. Defaults to `false`. Certain limitations apply and can be found [here](https://help.fullstory.com/hc/en-us/articles/360020622514-Can-FullStory-capture-content-that-is-presented-in-iframes-#h_01F1G333PYKPGZ4B42WDBV3YKV). 
  * - recordOnlyThisIFrame: FullStory can record the iFrame as its own unique session. Defaults to `false`. Additional conditions apply and can be found [here](https://help.fullstory.com/hc/en-us/articles/360020622514-Can-FullStory-capture-content-that-is-presented-in-iframes-#h_01F1G33B40Q2TPQA8MA7SF8Y5P).
