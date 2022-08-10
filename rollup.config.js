962e75cd0b626cebde2169f6576a57cc82f74467
Scott Norvell <scottlnorvell@gmail.com>
2022-08-10 15:50:06 -0400

    use @fullstory/snippet in place of snippet api

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,32 @@
 import babel from '@rollup/plugin-babel';
-import pkg from './package.json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
 
 export default [
-	{
-		input: 'src/index.js',
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'esm' }
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**'],
-        babelHelpers: 'bundled'
-			}),
-			copy({
-				targets: [
-					{ src: 'src/index.d.ts', dest: 'dist' },
-				]
-			}),
-		]
-	}
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'esm' },
+    ],
+    plugins: [
+      nodeResolve(),
+      babel({
+        exclude: ['node_modules/**'],
+        babelHelpers: 'bundled',
+      }),
+      copy({
+        targets: [{ src: 'src/index.d.ts', dest: 'dist' }],
+      }),
+      terser({
+        mangle: false,
+        compress: {
+          defaults: false,
+          dead_code: true,
+        },
+      }),
+    ],
+  },
 ];
