37a8d62ac5443eb9554e8d70574baa39eaed4e6e
Scott Norvell <scottlnorvell@gmail.com>
2023-02-08 15:51:24 -0500

    use dependency instead of devDependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import babel from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
-import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
@@ -11,21 +9,14 @@ export default [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'esm' },
     ],
+    external: ['@fullstory/snippet'],
     plugins: [
-      nodeResolve(),
       babel({
         exclude: ['node_modules/**'],
         babelHelpers: 'bundled',
       }),
       copy({
         targets: [{ src: 'src/index.d.ts', dest: 'dist' }],
-      }),
-      terser({
-        mangle: false,
-        compress: {
-          defaults: false,
-          dead_code: true,
-        },
       }),
     ],
   },
