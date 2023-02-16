da0028701252e043cf4a0c98bbf33b4531893a22
Daniel Whatley <danielwhatley@fullstory.com>
2023-02-16 11:36:44 -0500

    Add cookieDomain parameter to support setting window['_fs_cookie_domain']

--- src/snippet.js
+++ src/snippet.js
@@ -6,7 +6,8 @@ const snippet = (
     namespace = 'FS',
     debug = false,
     host = 'fullstory.com',
-    script = 'edge.fullstory.com/s/fs.js'
+    script = 'edge.fullstory.com/s/fs.js',
+    cookieDomain = '',
   }
 ) => {
   if (!orgId) {
@@ -17,6 +18,9 @@ const snippet = (
 window['_fs_script'] = script;
 window['_fs_org'] = orgId;
 window['_fs_namespace'] = namespace;
+if (cookieDomain !== '') {
+  window['_fs_cookie_domain'] = cookieDomain
+}
 (function(m,n,e,t,l,o,g,y){
     if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
     g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
