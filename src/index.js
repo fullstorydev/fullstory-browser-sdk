34fd99405b8e0595c3f3ced707bea0d32ba2a951
Scott Norvell <scottlnorvell@gmail.com>
2023-04-05 15:03:45 -0400

    Updating browser-sdk to v2

--- src/index.js
+++ src/index.js
@@ -1,48 +1,20 @@
 import { initFS } from '@fullstory/snippet';
 
-const fs = () => window[window._fs_namespace];
+const getFullStory = () => window[window._fs_namespace];
 
 const ensureSnippetLoaded = () => {
-  const snippetLoaded = !!fs();
-  if (!snippetLoaded) {
+  const fs = getFullStory();
+  if (!fs) {
     throw Error('FullStory is not loaded, please ensure the init function is invoked before calling FullStory API functions');
   }
+
+  return fs;
 };
-
-const hasFullStoryWithFunction = (...testNames) => {
-  ensureSnippetLoaded();
-  return testNames.every((current) => fs()[current]);
-};
-
-const guard = (name) => (...args) => {
-  if (window._fs_dev_mode) {
-    const message = `FullStory is in dev mode and is not recording: ${name} method not executed`;
-    console.warn(message);
-    return message;
-  }
-
-  if (hasFullStoryWithFunction(name)) {
-    return fs()[name](...args);
-  }
-  console.warn(`FS.${name} not ready`);
-  return null;
-};
-
-export const event = guard('event');
-export const log = guard('log');
-export const getCurrentSessionURL = guard('getCurrentSessionURL');
-export const identify = guard('identify');
-export const setUserVars = guard('setUserVars');
-export const consent = guard('consent');
-export const shutdown = guard('shutdown');
-export const restart = guard('restart');
-export const anonymize = guard('anonymize');
-export const setVars = guard('setVars');
 
 const _init = (inputOptions, readyCallback) => {
   // Make a copy so we can modify `options` if desired.
   const options = { ...inputOptions };
-  if (fs()) {
+  if (getFullStory()) {
     console.warn('The FullStory snippet has already been defined elsewhere (likely in the <head> element)');
     return;
   }
@@ -72,16 +44,21 @@ const _init = (inputOptions, readyCallback) => {
 
   initFS(options);
 
+  const fs = getFullStory();
+
   if (readyCallback) {
-    fs()('observe', { type: 'start', callback: readyCallback });
+    fs('observe', { type: 'start', callback: readyCallback });
   }
 
   if (options.devMode === true) {
     const message = 'FullStory was initialized in devMode and will stop recording';
-    event('FullStory Dev Mode', {
-      message_str: message,
+    fs('trackEvent', {
+      name: 'FullStory Dev Mode',
+      properties: {
+        message_str: message,
+      }
     });
-    shutdown();
+    fs('shutdown');
     window._fs_dev_mode = true;
     console.warn(message);
   }
@@ -100,3 +77,17 @@ export const init = initOnce(_init, 'FullStory init has already been called once
 
 // normalize undefined into boolean
 export const isInitialized = () => !!window._fs_initialized;
+
+const FullStory = (...args) => {
+  const fs = ensureSnippetLoaded();
+
+  if (window._fs_dev_mode) {
+    const message = 'FullStory is in dev mode and is not recording: method not executed';
+    console.warn(message);
+    return message;
+  }
+
+  return fs(...args);
+};
+
+export default FullStory;
--- test/index.js
+++ test/index.js
@@ -1,18 +1,7 @@
 import { assert, expect } from 'chai';
-import * as FullStory from '../src';
+import FS, { init, isInitialized } from '../src';
 
 const testOrg = '123';
-
-const snippetFunctions = ['anonymize',
-  'event',
-  'log',
-  'getCurrentSessionURL',
-  'identify',
-  'setUserVars',
-  'setVars',
-  'consent',
-  'shutdown',
-  'restart'];
 
 beforeEach(() => {
   if (window[window._fs_namespace]) {
@@ -23,34 +12,29 @@ beforeEach(() => {
   }
 });
 
-describe('core', () => {
-  it('should define browser API functions', () => {
-    const functions = ['init',
-      ...snippetFunctions
-    ];
-
-    functions.forEach((i) => assert(typeof FullStory[i] === 'function', `${i} has not been exported from the FullStory module`));
-  });
-});
-
 describe('init', () => {
   it('should throw error if not initialized with an orgId', () => {
-    expect(() => { FullStory.init(); }).to.throw();
+    expect(() => { init(); }).to.throw();
   });
 
   it('should throw error if API called before init', () => {
-    expect(() => { FullStory.log(); }).to.throw();
-    FullStory.init({ orgId: testOrg });
-    expect(() => { FullStory.log(); }).to.not.throw();
+    try {
+      FS('log', { msg: 'my log' });
+      expect(false).to.be('this should have thrown');
+    } catch (error) {
+      expect(error.message).to.match(/already been defined/);
+    }
+    init({ orgId: testOrg });
+    expect(() => { FS('log', { msg: 'my log' }); }).to.not.throw();
   });
 
   it('should add _fs_org value to window object', () => {
-    FullStory.init({ orgId: testOrg });
+    init({ orgId: testOrg });
     expect(window._fs_org).to.equal(testOrg);
   });
 
   it('should add _fs_run_in_iframe value to window object', () => {
-    FullStory.init({
+    init({
       orgId: testOrg,
       recordCrossDomainIFrames: true,
     });
@@ -58,7 +42,7 @@ describe('init', () => {
   });
 
   it('should add _fs_is_outer_script value to window object', () => {
-    FullStory.init({
+    init({
       orgId: testOrg,
       recordOnlyThisIFrame: true,
     });
@@ -66,7 +50,7 @@ describe('init', () => {
   });
 
   it('should add _fs_dev_mode value to window when initialzed with devMode', () => {
-    FullStory.init({
+    init({
       orgId: testOrg,
       devMode: true,
     });
@@ -75,19 +59,19 @@ describe('init', () => {
   });
 
   it('should return whether initialized', () => {
-    let isInit = FullStory.isInitialized();
+    let isInit = isInitialized();
     expect(isInit).to.equal(false);
 
-    FullStory.init({
+    init({
       orgId: testOrg,
     });
 
-    isInit = FullStory.isInitialized();
+    isInit = isInitialized();
     expect(isInit).to.equal(true);
   });
 
   it('should load fs-debug.js when debug is set', () => {
-    FullStory.init({
+    init({
       orgId: testOrg,
       debug: true,
     });
@@ -97,24 +81,22 @@ describe('init', () => {
 });
 
 describe('devMode', () => {
-  it('should return a message for all functions invoked when in devMode', () => {
-    FullStory.init({
+  it('should return a message for functions invoked when in devMode', () => {
+    init({
       orgId: testOrg,
       devMode: true,
     });
 
-    const returnValues = snippetFunctions.map((f) => FullStory[f]());
-
-    expect(returnValues.every((v) => typeof v === 'string')).to.equal(true);
+    expect(FS('log', { msg: 'hello world' })).to.match(/FullStory is in dev mode/);
   });
 });
 
 describe('getCurrentSessionURL', () => {
   it('should return null before fs.js is fully bootstrapped', () => {
-    FullStory.init({ orgId: testOrg });
+    init({ orgId: testOrg });
     // in theory, this is a race condition - assuming that fs.js
     // can't load by the time the following statement is executed
-    const url = FullStory.getCurrentSessionURL();
+    const url = FS('getSession');
     assert.equal(url, null, 'FullStory.getCurrentSessionURL() should return null if executed before fs.js is fully bootstrapped');
   });
 });
